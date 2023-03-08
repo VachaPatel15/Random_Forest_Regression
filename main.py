166243fb8be89f2143f683ecabd5a9a566200b84
VachaPatel15 <vachapatel8765@gmail.com>
2023-03-08 15:30:24 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+dataset = pd.read_csv('Position_Salaries.csv')
+x = dataset.iloc[:, 1:-1].values
+y = dataset.iloc[:, -1].values
+
+from sklearn.ensemble import RandomForestRegressor
+regressor = RandomForestRegressor(n_estimators=10, random_state=0)
+regressor.fit(x, y)
+
+y_pred = regressor.predict([[6.5]])
+# print(y_pred)
+
+x_grid = np.arange(min(x), max(x), 0.01)
+x_grid = x_grid.reshape(len(x_grid), 1)
+plt.scatter(x, y, color='red')
+plt.plot(x_grid, regressor.predict(x_grid), color='blue')
+plt.title('Random forest Regression')
+plt.xlabel('Position Level')
+plt.ylabel('Salary')
+plt.show()
+
